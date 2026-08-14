45d225dc4c2157930f3a9dd6af9fa3ec026d6ce5
Chris perspective0labs <perspective.labs@gmail.com>
2026-08-15 01:35:16 +0800

    Restrict Kasm schema versions list to schema branches

--- build_all_branches.sh
+++ build_all_branches.sh
@@ -11,8 +11,8 @@ touch base/.nojekyll
 # Generating documentation for each other branch in a subdirectory
 echo "All branches:"
 git fetch --all
-echo "$(git branch --remotes --format '%(refname:lstrip=3)' | grep -Ev '^(HEAD|develop|gh-pages)$')"
-for BRANCH in $(git branch --remotes --format '%(refname:lstrip=3)' | grep -Ev '^(HEAD|develop|gh-pages)$'); do
+echo "$(git branch --remotes --format '%(refname:lstrip=3)' | grep -E '^[0-9]+\.[0-9]+$')"
+for BRANCH in $(git branch --remotes --format '%(refname:lstrip=3)' | grep -E '^[0-9]+\.[0-9]+$'); do
     SANITIZED_BRANCH="$(echo $BRANCH | sed 's/\//_/g')"
     echo "$SANITIZED_BRANCH" >> base/versions.txt
     git checkout --force $BRANCH
